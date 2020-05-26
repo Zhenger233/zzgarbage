ae5f92e87b716a8bb0577b65d7e2b1ed331b0c1f
张征 <2042712521@qq.com>
2020-05-26 22:56:00 +0800

    restart
    lc1

--- zz.cpp
+++ zz.cpp
@@ -5713,3 +5713,20 @@ int main()
     return 0;
 }
 */
+/*leetcode 1
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int l=nums.size();
+        vector<int> v;
+        for(int i=0;i<l;i++){
+            for(int j=i+1;j<l;j++){
+                if(nums[i]+nums[j]==target){
+                    v.push_back(i);v.push_back(j);
+                }
+            }
+        }
+        return v;
+    }
+};
+*/
